fd922af9de5f00c804d69d4aa712476a3fea50f6
front-citi <israeldesignerce@outlook.com>
2023-04-05 17:03:50 -0300

    dynamic values leaflet

--- frontend/js/modules/leaflet.js
+++ frontend/js/modules/leaflet.js
@@ -21,51 +21,7 @@ const leaflet =() =>{
     let blocoTexto;
     const _radius = 250;
     const _fillOpacity = 0.5;
-    const checkElement = (elementValue, arrayElements, local,elementSimbolo, elementName) =>{
-
-        if(elementValue < arrayElements[0]) {
-            nValue =`<b>NÍVEL N1</b> - qualidade boa 🤩`;
-            colorForPm2 = { color:'Green', fillColor: '#adff2f',fillOpacity: _fillOpacity, radius: _radius };
-        } 
-        if(elementValue > arrayElements[1] && elementValue < arrayElements[1]) {
-            nValue = `<b>NÍVEL N2</b> - qualidade Moderada 😀`;
-            colorForPm2 = { color:'yellow', fillColor: '#ffc801',fillOpacity: _fillOpacity, radius: _radius };
-        }
-        if(elementValue > arrayElements[2] && elementValue < arrayElements[2]) {
-            nValue = `<b>NÍVEL N3</b> - qualidade Ruim 🤨`;
-            colorForPm2 = { color:'orange', fillColor: '#ffcd03',fillOpacity: _fillOpacity, radius: _radius };
-        }
-        if(elementValue > arrayElements[3] && elementValue < arrayElements[3]) {
-            nValue = `<b>NÍVEL N4</b> - Muito Ruim 🙁`;
-            colorForPm2 = { color:'red', fillColor: '#f03',fillOpacity: _fillOpacity, radius: _radius };
-        }
-        if(elementValue > arrayElements[4] && elementValue < arrayElements[4]){
-            nValue = `<b>NÍVEL N5</b> - Péssima 😡`;
-            colorForPm2 = { color:'purple', fillColor: '#c904c9',fillOpacity: _fillOpacity, radius: _radius };
-        } 
-        if(elementValue > arrayElements[4]) {
-            nValue = `<b>NÍVEL N5</b> - Péssima 😡`;
-            colorForPm2 = { color:'purple', fillColor: '#c904c9',fillOpacity: _fillOpacity, radius: _radius };
-        };
-
-        console.log(colorForPm2)
-        console.log(nValue)
-        console.log(elementValue)
-        console.log(arrayElements)
-        console.log(elementSimbolo)
-        console.log(local);
-
-        blocoTexto = 
-        `
-        <h2 id="localPopUp"style="font-size:2.0rem;">${local}</h2>
-        <br>
-        <b>${elementSimbolo}/m3 - ${elementValue}</b> 
-        <p style="font-size:1.0rem">${nValue}</p>
-        <p> ${elementName}</p>
-        <hr style=" border-bottom:1px solid #b2b2b2;"/>
-        `;
-    }
-
+    
     const fetchApiData = async () => {
         let methodsFetc = {
             method: 'GET',
@@ -81,33 +37,91 @@ const leaflet =() =>{
             }
             
             const getPointsData = () =>{
-                console.log(responseData);
                 let arrayLastItems =[]
                 for (const key in responseData) {
                     if (Object.hasOwnProperty.call(responseData, key)) {
                         const element = responseData[key];
                         let lastElement = element[element.length -1];
 
-                        let pLocal = lastElement.Local;
-                        let latitude = lastElement.Latitude;
-                        let longitude = lastElement.Longitude;
-                        let humidity = lastElement.Humidity;
-                        let coppm = lastElement.COppm;
-                        let pm2ug = lastElement.PM2ug;
-                        let pm10ug = lastElement.PM10ug;
-                        let exTmp = lastElement.ExTemp;
-                        let no2ug = lastElement.NO2ug;
-                        let o3ug = lastElement.O3ug;
+                        let _pLocal = lastElement.Local;
+                        let _latitude = lastElement.Latitude;
+                        let _longitude = lastElement.Longitude;
+                        let _humidity = lastElement.Humidity;
+                        let _coppm = lastElement.COppm;
+                        let _pm2ug = lastElement.PM2ug;
+                        let _pm10ug = lastElement.PM10ug;
+                        let _exTmp = lastElement.ExTemp;
+                        let _no2ug = lastElement.NO2ug;
+                        let _o3ug = lastElement.O3ug;
 
                         const arrayLatLong = [lastElement.Latitude, lastElement.Longitude];
-                        const arrayIndicesPm2u = [25, 50, 75, 125, 300]
+                        const _arrayIndicesPm2u  = [25, 50, 75, 125, 300];
+                        const _arrayIndicesPm10 = [50,100,150,250,600];
+                        const _arrayIndicesCoppm = [10, 12, 14, 16, 50];
+                        const _arrayIndicesNo2 = [200,240,320,1130,3750];
+                        const _arrayIndicesO3 = [100,130,160,200,800];
+                        const _arraySymbols = ['PM2.5','PM10','CO' ,'NO2','O3'];
+                        const _arrayElements = [_pm2ug, _pm10ug, _coppm, _no2ug, _o3ug];
+                        const objectTestEl = { 
+                            pLocal:_pLocal,
+                            humidity: _humidity,
+                            coppm:_coppm,
+                            pm2ug:_pm2ug,
+                            pm10ug:_pm10ug,
+                            exTmp:_exTmp,
+                            no2ug:_no2ug,
+                            o3ug:_o3ug
+                        }
+                        
+                        const cheElementsInd = (elementValue, arrayElements, elementName, elementLocal) =>{
+                            if(elementValue < arrayElements[0]) {
+                                nValue =`<b>NÍVEL N1</b> - qualidade boa 🤩`;
+                                colorForPm2 = { color:'Green', fillColor: '#adff2f',fillOpacity: _fillOpacity, radius: _radius };
+                            } 
+                            if(elementValue > arrayElements[0] && elementValue < arrayElements[1]) {
+                                nValue = `<b>NÍVEL N2</b> - qualidade Moderada 😀`;
+                                colorForPm2 = { color:'yellow', fillColor: '#ffc801',fillOpacity: _fillOpacity, radius: _radius };
+                            }
+                            if(elementValue > arrayElements[1] && elementValue < arrayElements[2]) {
+                                nValue = `<b>NÍVEL N3</b> - qualidade Ruim 🤨`;
+                                colorForPm2 = { color:'orange', fillColor: '#ffcd03',fillOpacity: _fillOpacity, radius: _radius };
+                            }
+                            if(elementValue > arrayElements[2] && elementValue < arrayElements[3]) {
+                                nValue = `<b>NÍVEL N4</b> - Muito Ruim 🙁`;
+                                colorForPm2 = { color:'red', fillColor: '#f03',fillOpacity: _fillOpacity, radius: _radius };
+                            }
+                            if(elementValue > arrayElements[3] && elementValue < arrayElements[4]){
+                                nValue = `<b>NÍVEL N5</b> - Péssima 😡`;
+                                colorForPm2 = { color:'purple', fillColor: '#c904c9',fillOpacity: _fillOpacity, radius: _radius };
+                            } 
+                            if(elementValue > arrayElements[4]) {
+                                nValue = `<b>NÍVEL N5</b> - Péssima 😡`;
+                                colorForPm2 = { color:'purple', fillColor: '#c904c9',fillOpacity: _fillOpacity, radius: _radius };
+                            };
 
-                        checkElement(pm2ug, arrayIndicesPm2u, pLocal, 'PM2.5', 'Ponoxido')
-                        console.log(colorForPm2);
+                            blocoTexto = 
+                            `
+                            <h2 id="localPopUp"style="font-size:2.0rem;">${_pLocal}</h2>
+                            <br>
+                            <b>${elementName}/m3 - ${elementValue}</b> 
+                            <p style="font-size:1.0rem">${nValue}</p>
+                            <b>Humidade: ${_humidity}</b>
+                            <br>
+                            <b>O3/m3 - ${_o3ug}</b> 
+                            <p style="font-size:1.0rem">${nValue}</p>
+                            <br>
+                            <b>NO2/m3 - ${_no2ug}</b> 
+                            <p style="font-size:1.0rem">${nValue}</p>
+                            <br>
+                            <b>PM10/m3 - ${_pm10ug}</b> 
+                            <p style="font-size:1.0rem">${nValue}</p>
+                            `;
+                        }
 
-                        // if(pm2ug < 7) colorForPm2 = { color:'red', fillColor: '#f03',fillOpacity: 0.5, radius: 500 };
-                        // if(pm2ug > 8) colorForPm2 = { color:'yellow', fillColor: '#ffc801',fillOpacity: 0.5, radius: 500 };
-
+                        
+                        cheElementsInd(_pm2ug, _arrayIndicesPm2u, 'PM2.5', _pLocal);
+                        // cheElementsInd(_coppm, _arrayIndicesCoppm, 'COPPM', _pLocal);
+                        L.circle(arrayLatLong, colorForPm2).addTo(map).bindPopup(blocoTexto).openPopup();
                         // let markerPoint = L.icon({
                         //     iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-green.png',
                         //     iconRetinaUrl: 'https://leafletjs.com/examples/custom-icons/leaf-green.png',
@@ -124,8 +138,6 @@ const leaflet =() =>{
                         // L.Icon.Default.imagePath = "https://leafletjs.com/examples/custom-icons/leaf-green.png";
                         // L.Icon.Default.className = "https://leafletjs.com/examples/custom-icons/leaf-green.png";
                         // L.marker(arrayLatLong, markerPoint ).addTo(map).bindPopup(`${lastElement.Local}`).openPopup();
-
-                        L.circle(arrayLatLong, colorForPm2).addTo(map).bindPopup(`${blocoTexto}`).openPopup();
                     }
                 }
             }
